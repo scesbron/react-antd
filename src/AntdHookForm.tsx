060a36a3135e18638c116bb1083f42d0456f15b9
Sébastien Cesbron <sebastien.cesbron@traace.co>
2024-11-29 14:18:26 +0100

    use hook form useWatch

--- src/AntdHookForm.tsx
+++ src/AntdHookForm.tsx
@@ -1,5 +1,5 @@
 import {Button, Form, Input} from "antd";
-import {SubmitHandler, useForm} from "react-hook-form";
+import {FormProvider, SubmitHandler, useForm, useWatch} from "react-hook-form";
 import {FormItem} from "react-hook-form-antd";
 
 type FormValues = {
@@ -7,26 +7,33 @@ type FormValues = {
   description?: string
 }
 
+function InsideForm() {
+  const name = useWatch<FormValues>({ name: 'name'})
+  const description = useWatch<FormValues>({ name: 'description'})
+
+  console.log('inside watch name', name)
+  console.log('inside watch description', description)
+  return null
+}
+
 export default function AntdHookForm() {
-  const {
-    control,
-    handleSubmit,
-    watch,
-  } = useForm<FormValues>({ defaultValues: {
+  const methods = useForm<FormValues>({ defaultValues: {
       name: 'test'
     }})
   const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data)
-  const name = watch("name")
-  const description = watch("description")
+  const name = methods.watch("name")
+  const description = methods.watch("description")
 
   console.log("name", name)
   console.log("description", description)
 
   return (
-    <Form onFinish={handleSubmit(onSubmit)}>
-      <FormItem control={control} name="name"><Input /></FormItem>
+    <FormProvider {...methods}>
+    <Form onFinish={methods.handleSubmit(onSubmit)}>
+      <InsideForm />
+      <FormItem control={methods.control} name="name"><Input /></FormItem>
       <Form.Item name="description"><Input /></Form.Item>
       <Button htmlType="submit" type="primary">Submit</Button>
-    </Form>
+    </Form></FormProvider>
   )
 }
