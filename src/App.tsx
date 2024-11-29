1c90f126dfa70205c989b815261dd0c60e06a306
Sébastien Cesbron <sebastien.cesbron@traace.co>
2024-11-29 10:16:32 +0100

    update form test

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,25 @@ type FormValues = {
 }
 
 function InsideForm() {
-  const form = Form.useFormInstance<FormValues>()
-  const watchedName = Form.useWatch('name')
-  const name = form.getFieldValue('name')
-  console.log('the name is ', name)
-  console.log('the watched name inside form is ', watchedName)
-
-  return <div>The name is : {name}</div>
+  const form = Form.useFormInstance()
+  const name = Form.useWatch('name')
+  const description = Form.useWatch('description')
+  console.log('the field value for name inside form is ', form.getFieldValue('name'))
+  console.log('the watched name inside form is ', name)
+  console.log('the watched description inside form is ', description)
+  return null
 }
 
 function App() {
   const [form] = Form.useForm<FormValues>()
-  const watchedName = Form.useWatch('name')
+  const name = Form.useWatch('name')
+  const description = Form.useWatch('description')
 
   const submit = (values: FormValues) => {
     console.log('submit', values)
   }
-  console.log('the watched name outside form is ', watchedName)
+  console.log('the watched name outside form is ', name)
+  console.log('the watched description outside form is ', description)
   return (
     <Form form={form} onFinish={submit} initialValues={{ name: ''}}>
       <InsideForm />
